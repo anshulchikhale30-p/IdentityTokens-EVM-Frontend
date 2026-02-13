56af190b9eb7786d31f761320ba80506ac296f3b
anshulchikhale30p <anshulchikhale30p@gmail.com>
2026-02-13 09:37:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,48 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Mini Decentralized Identity Tokens",
+  title: "Decentralized Identity Tokens",
   description: "Portable, Recoverable and Self-Sovereign Identity",
+  icons: {
+    icon: "/logo.svg",
+  },
 };
+
+const atypFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/AtypDisplayTRIAL-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-atyp",
+  display: "swap",
+});
+
+const garamondFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/Garamond.woff2",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  variable: "--font-garamond",
+  display: "swap",
+});
+
+const utsahaFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/UtSaHaGumm.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-utsaha",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +53,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${atypFont.variable} ${garamondFont.variable} ${utsahaFont.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
