4eb3ead28a8d0868933a4334768d3adb5d9cbb2d
anshulchikhale30p <anshulchikhale30p@gmail.com>
2026-02-13 09:06:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,65 +1,15 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import Navbar from "@/components/Navbar";
-import "./globals.css";
+import { Hero } from "@/components/Hero";
+import { HowItWorks } from "@/components/HowItWorks";
+import { FeatureSection } from "@/components/FeatureSection";
+import CTASection from "@/components/CTASection";
 
-const atypFont = localFont({
-  src: [
-    {
-      path: "../public/fonts/AtypDisplayTRIAL-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-atyp",
-  display: "swap",
-});
-
-const garamondFont = localFont({
-  src: [
-    {
-      path: "../public/fonts/Garamond.woff2",
-      weight: "400",
-      style: "italic",
-    },
-  ],
-  variable: "--font-garamond",
-  display: "swap",
-});
-
-const utsahaFont = localFont({
-  src: [
-    {
-      path: "../public/fonts/UtSaHaGumm.woff2",
-      weight: "400",
-      style: "normal",
-    },
-  ],
-  variable: "--font-utsaha",
-  display: "swap",
-});
-
-export const metadata: Metadata = {
-  title: "Decentralized Identity Token",
-  description: "Portable, Recoverable and Self-Sovereign Identity",
-  icons: {
-    icon: "/assets/logo.svg",
-  },
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Home() {
   return (
-    <html lang="en">
-      <body
-        className={`${atypFont.variable} ${garamondFont.variable} ${utsahaFont.variable} antialiased bg-gray-100`}
-      >
-        <Navbar />
-        <main className="max-w-4xl mx-auto p-6">{children}</main>
-      </body>
-    </html>
+    <>
+      <Hero />
+      <HowItWorks />
+      <FeatureSection />
+      <CTASection />
+    </>
   );
 }
