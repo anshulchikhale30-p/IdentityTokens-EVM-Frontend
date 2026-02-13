c73433f9720545e907d1dada69b69a2e25dee0c1
anshulchikhale30p <anshulchikhale30p@gmail.com>
2026-02-13 08:51:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.BASE_PATH || "";
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // Required for GitHub Pages
-  basePath: basePath,
-  images: {
-    unoptimized: true, // Required for static export
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
